4dad88cf2925e3fd47fbca023c6ce61a061df590
Israel Felipe <israelsimplicio0@gmail.com>
2020-12-03 15:27:20 -0300

    Criação dos services

--- blogPessoal-ui/src/app/perfil-lateral/perfil-lateral.component.ts
+++ blogPessoal-ui/src/app/perfil-lateral/perfil-lateral.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilLateralComponent implements OnInit {
 
+  nome: string = 'Israel Felipe'
+
   constructor() { }
-
-  ngOnInit(): void {
+  
+  ngOnInit() {
+    
   }
-
 }
